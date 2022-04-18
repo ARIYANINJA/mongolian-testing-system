408572f9eed1e2d33929ca540de120ca5f41a79b
Ariyaninj <ariyaninj@gmail.com>
2022-04-18 16:20:28 +0800

    doneCountdown

--- src/components/countdown.vue
+++ src/components/countdown.vue
@@ -12,15 +12,16 @@ mounted(){
   const startingMinutes = 1;
   let time = startingMinutes * 60;
   const countdownEl = document.getElementById('countdown');
-   setInterval(updateCountdown,1000);
+  const myInverval = setInterval(updateCountdown,1000);
   function updateCountdown(){
      let minutes = Math.floor(time / 60);
      let seconds = time % 60; 
      seconds = seconds < 10 ? '0' + seconds : seconds;
      countdownEl.innerHTML = `${minutes} : ${seconds}`;
+     if(time <= 0 ){
+       clearInterval(myInverval);}
      time--;
   }
- 
 }
 }
 </script>
